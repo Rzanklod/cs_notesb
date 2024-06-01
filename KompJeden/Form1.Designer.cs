8a7e2addec47575860f080d2a8a0a1f8296d816e
Rzanklod <malwinaxxx666@gmail.com>
2024-06-01 17:16:57 +0200

    Add project files.

---
+++ KompJeden/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace KompJeden
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxMain = new System.Windows.Forms.ListBox();
+            this.textBoxPreviewName = new System.Windows.Forms.TextBox();
+            this.richTextBoxPreviewContent = new System.Windows.Forms.RichTextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxMain
+            // 
+            this.listBoxMain.FormattingEnabled = true;
+            this.listBoxMain.Location = new System.Drawing.Point(16, 8);
+            this.listBoxMain.Name = "listBoxMain";
+            this.listBoxMain.Size = new System.Drawing.Size(112, 420);
+            this.listBoxMain.TabIndex = 0;
+            this.listBoxMain.SelectedIndexChanged += new System.EventHandler(this.listBoxMain_SelectedIndexChanged);
+            // 
+            // textBoxPreviewName
+            // 
+            this.textBoxPreviewName.Location = new System.Drawing.Point(157, 12);
+            this.textBoxPreviewName.Name = "textBoxPreviewName";
+            this.textBoxPreviewName.Size = new System.Drawing.Size(146, 20);
+            this.textBoxPreviewName.TabIndex = 1;
+            // 
+            // richTextBoxPreviewContent
+            // 
+            this.richTextBoxPreviewContent.Location = new System.Drawing.Point(157, 54);
+            this.richTextBoxPreviewContent.Name = "richTextBoxPreviewContent";
+            this.richTextBoxPreviewContent.Size = new System.Drawing.Size(146, 281);
+            this.richTextBoxPreviewContent.TabIndex = 2;
+            this.richTextBoxPreviewContent.Text = "";
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(209, 342);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(52, 36);
+            this.buttonSubmit.TabIndex = 5;
+            this.buttonSubmit.Text = "New";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(157, 343);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(46, 36);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(267, 345);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(37, 34);
+            this.buttonEdit.TabIndex = 7;
+            this.buttonEdit.Text = "Edit";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            this.buttonEdit.Click += new System.EventHandler(this.buttonEdit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 434);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.richTextBoxPreviewContent);
+            this.Controls.Add(this.textBoxPreviewName);
+            this.Controls.Add(this.listBoxMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxMain;
+        private System.Windows.Forms.TextBox textBoxPreviewName;
+        private System.Windows.Forms.RichTextBox richTextBoxPreviewContent;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonEdit;
+    }
+}
+
---
+++ KompJeden/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KompJeden
+{
+    public partial class Form1 : Form
+    {
+        NotesDataContext ndc;
+        public Form1()
+        {
+            ndc = new NotesDataContext();
+            InitializeComponent();
+            foreach(Note n in ndc.Notes)
+            {
+                listBoxMain.Items.Add(n);
+            }
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            string name = textBoxPreviewName.Text;
+            if (name.Length == 0 || name.Length > 40) return;
+
+            string description = richTextBoxPreviewContent.Text;
+            if (description.Length == 0 || description.Length > 400) return;
+
+            Note newNote = new Note();
+            newNote.Name = name;
+            newNote.Description = description;
+
+            ndc.Notes.InsertOnSubmit(newNote);
+            listBoxMain.Items.Add(newNote);
+            ndc.SubmitChanges();
+
+            if(!(listBoxMain.SelectedItem is Note)) setPreviewText("", "");
+        }
+
+        private void listBoxMain_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(!(listBoxMain.SelectedItem is Note))
+            {
+                setPreviewText("", "");
+                return;
+            }
+
+            Note selectedNote = (listBoxMain.SelectedItem as Note);
+            setPreviewText(selectedNote.Name, selectedNote.Description);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (!(listBoxMain.SelectedItem is Note)) return;
+
+            Note note = (listBoxMain.SelectedItem as Note);
+            listBoxMain.Items.Remove(note);
+            ndc.Notes.DeleteOnSubmit(note);
+            ndc.SubmitChanges();
+            setPreviewText("", "");
+        }
+
+        private void setPreviewText(string name, string desc)
+        {
+            textBoxPreviewName.Text = name;
+            richTextBoxPreviewContent.Text = desc;
+        }
+
+        private void buttonEdit_Click(object sender, EventArgs e)
+        {
+            if (!(listBoxMain.SelectedItem is Note)) return;
+
+            string name = textBoxPreviewName.Text;
+            if (name.Length == 0 || name.Length > 40) return;
+
+            string description = richTextBoxPreviewContent.Text;
+            if (description.Length == 0 || description.Length > 400) return;
+
+            Note note = (listBoxMain.SelectedItem as Note);
+            note.Name = name;
+            note.Description = description;
+            ndc.SubmitChanges();
+            listBoxMain.Items[listBoxMain.SelectedIndex] = note;
+        }
+    }
+}
